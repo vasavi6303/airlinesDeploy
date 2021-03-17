a8302251cc9faeb4f942e3792c6f229ce82aebeb
vasavi6303 <vasavisai333@gmail.com>
2021-03-17 11:30:24 +0530

    added changes

--- src/test/java/com/ibm/abcairlines/repository/FlightRepositoryTest.java
+++ src/test/java/com/ibm/abcairlines/repository/FlightRepositoryTest.java
@@ -1,32 +1,90 @@
-//package com.ibm.abcairlines.repository;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.sql.Date;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import com.ibm.abcairlines.entity.Flight;
-//
-//@SpringBootTest
-//
-//public class FlightRepositoryTest {
-//
-//	@Autowired
-//	private FlightRepository repository;
-//
-//	@Test
-//	@DisplayName("Test Flight not found for non existing id")
-//	public void testFlightNotFoundForNonExistingId() {
-//
-//		Flight retrievedFlight = repository.findBySourceAndDestinationAndDate("Pune", "Chennai",
-//				Date.valueOf("2021-04-02"));
-//
-//		assertEquals(1, retrievedFlight.getId());
-//
-//	}
-//
-//}
+package com.ibm.abcairlines.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.abcairlines.entity.Flight;
+
+@SpringBootTest
+public class FlightRepositoryTest {
+	@Autowired
+	private FlightRepository repository;
+
+	private static File DATA_JSON = Paths.get("resources", "flights.json").toFile();
+
+	@BeforeEach
+	void setup() throws JsonParseException, JsonMappingException, IOException {
+
+		Flight flights[] = new ObjectMapper().readValue(DATA_JSON, Flight[].class);
+		Arrays.stream(flights).forEach(repository::save);
+
+	}
+
+	@AfterEach
+
+	void tearDown() {
+
+		repository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("Test Flight not found for non existing id")
+	public void testFlightNotFoundForNonExistingDate() {
+
+		// given 4 flights in the database
+
+		// when
+		Flight retrievedFlight = repository.findBySourceAndDestinationAndDate("Pune", "Chennai",
+				Date.valueOf("2022-04-02"));
+
+		// then
+
+		assertNull(retrievedFlight, "Flight not available");
+
+	}
+
+	@Test
+	@DisplayName("Test Flight not available")
+	public void testFlightSavedSuccesfully() {
+
+		// Prepare Mock Product
+		Flight flight = new Flight();
+		flight.setId(1);
+		flight.setSource("Pune");
+		flight.setDestination("Chennai");
+		flight.setAvailableBusinessClassSeats(30);
+		flight.setAvailableEconomyClassSeats(30);
+
+		// when
+		Flight savedFlight = repository.save(flight);
+
+		// then
+
+		assertNotNull(savedFlight, "Flight should be saved");
+		assertNotNull(savedFlight.getId(), "Flight should have id after saving");
+		assertEquals(flight.getId(), 1);
+		assertEquals(flight.getSource(), "Pune");
+		assertEquals(flight.getDestination(), "Chennai");
+		assertEquals(flight.getAvailableBusinessClassSeats(), 30);
+		assertEquals(flight.getAvailableEconomyClassSeats(), 30);
+
+	}
+
+}
--- src/test/java/com/ibm/abcairlines/service/BookingServiceTest.java
+++ src/test/java/com/ibm/abcairlines/service/BookingServiceTest.java
@@ -1,11 +1,16 @@
 package com.ibm.abcairlines.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.ibm.abcairlines.common.Common.Gender;
+import com.ibm.abcairlines.common.Common.PreferredClass;
 import com.ibm.abcairlines.entity.Booking;
+import com.ibm.abcairlines.entity.Flight;
+import com.ibm.abcairlines.entity.Passenger;
 import com.ibm.abcairlines.repository.BookingRepository;
 
 @SpringBootTest
@@ -25,13 +34,14 @@ public class BookingServiceTest {
 	BookingRepository bookingRepository;
 
 	@Autowired
+
 	BookingService bookingService;
 
 	@Test
 	@DisplayName("Test getBookingById when booking ID found")
 	public void testGetBookingById() {
 		MockitoAnnotations.openMocks(this);
-		when(bookingRepository.findById(anyInt())).thenReturn(Optional.of(mock(Booking.class)));
+		when(bookingRepository.findById(anyInt())).thenReturn(Optional.of((mock(Booking.class))));
 		Booking booking = bookingService.getBookingById(1);
 		assertNotNull(booking);
 	}
@@ -42,7 +52,78 @@ public class BookingServiceTest {
 		Booking booking = bookingService.getBookingById(1);
 		assertNull(booking);
 	}
-	
-	
+
+	@Test
+	public void testGetFlights() {
+		Booking mockbooking1 = new Booking();
+		mockbooking1.setBookedDate(Date.valueOf("2021-03-24"));
+		mockbooking1.setId(1);
+		mockbooking1.setOnBoardingDate(Date.valueOf("2021-04-02"));
+		mockbooking1.setPreferredClass(PreferredClass.BUSINESSCLASS);
+		mockbooking1.setFlight(getflight());
+		mockbooking1.setPassenger(getPassenger1());
+		mockbooking1.setFare(5000);
+		Booking mockbooking2 = new Booking();
+		mockbooking2.setBookedDate(Date.valueOf("2021-03-26"));
+		mockbooking2.setId(2);
+		mockbooking2.setOnBoardingDate(Date.valueOf("2021-04-04"));
+		mockbooking2.setPreferredClass(PreferredClass.ECONOMYCLASS);
+		mockbooking2.setFlight(getflight());
+		mockbooking2.setPassenger(getPassenger1());
+		mockbooking2.setFare(2000);
+
+		doReturn(Arrays.asList(mockbooking1, mockbooking2)).when(bookingRepository).findAll();
+
+		Iterable<Booking> booking = bookingService.getBookings();
+		assertEquals(2, ((Collection<?>) booking).size());
+
+	}
+
+	public Passenger getPassenger1() {
+		Passenger passenger = new Passenger();
+		passenger.setName("Rohan");
+		passenger.setAge(56);
+		passenger.setContact(123);
+		passenger.setGender(Gender.MALE);
+
+		return passenger;
+	}
+
+	public Flight getflight() {
+		Flight mockFlight = new Flight();
+		mockFlight.setId(1);
+		mockFlight.setDate(Date.valueOf("2021-04-07"));
+		mockFlight.setDestination("Delhi");
+		mockFlight.setSource("Pune");
+		mockFlight.setAvailableBusinessClassSeats(0);
+		mockFlight.setAvailableEconomyClassSeats(30);
+		return mockFlight;
+	}
+
+	@Test
+	@DisplayName("Test Save booking")
+	public void testFlightSavedSuccesfully() {
+
+		Booking mockbooking1 = new Booking();
+		mockbooking1.setBookedDate(Date.valueOf("2021-03-24"));
+		mockbooking1.setId(1);
+		mockbooking1.setOnBoardingDate(Date.valueOf("2021-04-02"));
+		mockbooking1.setPreferredClass(PreferredClass.BUSINESSCLASS);
+		mockbooking1.setFlight(getflight());
+		mockbooking1.setPassenger(getPassenger1());
+		mockbooking1.setFare(5000);
+		// given 4 flights in the database
+
+		// when
+
+		doReturn(mockbooking1).when(bookingRepository).save(mockbooking1);
+		Booking savedbooking = bookingRepository.save(mockbooking1);
+		// then
+		System.out.println(savedbooking);
+		assertNotNull(savedbooking, "Booking should be saved");
+		assertNotNull(savedbooking.getId(), "Booking should get an id");
+		assertEquals(mockbooking1.getFlight(), savedbooking.getFlight());
+
+	}
 
 }
